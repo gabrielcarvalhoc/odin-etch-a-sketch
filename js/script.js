0c5e3ab007811be0242bdf6bc9284c5e38ed7fe0
Gabriel <gabrielcarvalho@poli.ufrj.br>
2022-06-22 17:19:29 -0300

    Add function to choose number of squares

--- js/script.js
+++ js/script.js
@@ -1,19 +1,43 @@
 let container = document.querySelector('.grid-container');
+let input = document.querySelector('#squares-input');
+let btn = document.querySelector('.btn-squares');
+
+let squaresPerSide = 16;
 
 function createGrid() {
-    for (let i = 1; i <= 16*16; i++) {
+    container.innerHTML = '';
+
+    container.style.gridTemplateColumns = 'auto '.repeat(squaresPerSide);
+
+    for (let i = 1; i <= squaresPerSide ** 2; i++) {
         let squareDiv = document.createElement('div');
         squareDiv.classList.add('grid-item');
         container.appendChild(squareDiv);
     }
 
-    const squareDivs = document.querySelectorAll('.grid-item');
+    function setUpHoverEffect() {
+        const squareDivs = document.querySelectorAll('.grid-item');
 
-    squareDivs.forEach((squareDiv) => {
-        squareDiv.addEventListener('mouseenter', () => {
-            squareDiv.style.backgroundColor = 'black';
+        squareDivs.forEach((squareDiv) => {
+            squareDiv.addEventListener('mouseenter', () => {
+                squareDiv.style.backgroundColor = 'black';
+            });
         });
+    }
+
+    setUpHoverEffect();
+}
+
+function setSquaresPerSide() {
+    btn.addEventListener('click', () => {
+        if (input.value <= 100) {
+            squaresPerSide = input.value;
+            createGrid();
+        } else {
+            swal('Oh No!', 'Please Choose a Number Less than 100.', 'error');
+        }
     });
 }
 
 createGrid();
+setSquaresPerSide();
